b3f571e385b2afa385e30c1d3b15e9cd49616b7e
Tijani Olawale <tijaniolawale99@gmail.com>
2024-03-27 18:05:38 -0400

    working on controlled input for the registration comp

--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -1,30 +1,69 @@
 import { useState } from "react";
 import { Form, Button, FormControl } from "react-bootstrap";
+import { Navigation, useNavigate } from "react-router-dom";
+import LogIn from "./LogIn";
+const Registration = () => {
+  const navigate = useNavigate();
+  const [inputValues, setInputValues] = useState({
+    userName: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    gender: "",
+  });
 
-function Registration() {
-  const [inputValues, setInputValues] = useState([
-    { userName: "", userNameErr: "" },
-    { firstName: "", firstNameErr: "" },
-    {lastName:"", lastNameErr:""},
-    {},
-    {},
-    {},
-  ]);
+  const [inputErrors, setInputErrors] = useState({
+    userName: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    gender: "",
+  });
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setInputValues((prevValues) => ({ ...prevValues, [name]: value }));
+  };
+
+  const handleBlur = ({ target }) => {
+    const { name, value } = target;
+    if (name === "userName" && value.length < 5) {
+      setInputErrors((prevError) => ({
+        ...prevError,
+        [name]: "Username must be at least five Characters",
+      }));
+    } else {
+      setInputErrors((prevError) => ({ ...prevError, [name]: "" }));
+    }
+  };
+  console.log(inputValues.userName);
 
   return (
-    <div>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
+    <div className="color-overlay d-flex justify-content-center align-items-center">
+      <Form onSubmit={(e) => e.preventDefault()}>
+        <Form.Group controlId="username">
           <Form.Label>Username</Form.Label>
-          <FormControl />
+          <FormControl
+            name="userName"
+            value={inputValues.userName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          {inputErrors.userName && (
+            <Form.Text className="text-danger">
+              {inputErrors.userName}
+            </Form.Text>
+          )}
         </Form.Group>
 
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group controlId="firstname">
           <Form.Label>First name</Form.Label>
-          <FormControl />
+          <FormControl name="firstname" value={inputValues.firstName} />
         </Form.Group>
 
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group controlId="lastname">
           <Form.Label>Last name</Form.Label>
           <FormControl />
         </Form.Group>
@@ -34,18 +73,35 @@ function Registration() {
           <FormControl />
         </Form.Group>
 
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <FormControl />
         </Form.Group>
 
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Gender</Form.Label>
-          <FormControl />
+        <Form.Group>
+          <Form.Label>Gender:</Form.Label>
+          <Form.Select>
+            <option>Open this selected menu</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Other">Other</option>
+          </Form.Select>
         </Form.Group>
+
+        <Form.Group className="mb-3" controlId="forBasicCheckbox">
+          <Form.Check type="checkbox" label="I agree to terms and conditions" />
+        </Form.Group>
+
+        <Button
+          id="register"
+          className="btn btn-dark"
+          onClick={() => navigate("/dashboard")}
+        >
+          Register
+        </Button>
       </Form>
     </div>
   );
-}
+};
 
 export default Registration;
