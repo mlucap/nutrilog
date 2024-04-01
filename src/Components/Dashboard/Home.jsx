fbe3d951d5ef6a549f1273d2648c6b227cecea68
Luca Pereira <pereluca10@gmail.com>
2024-04-01 16:54:35 -0400

    dashboard home page

--- src/Components/Dashboard/Home.jsx
+++ src/Components/Dashboard/Home.jsx
@@ -1,6 +1,51 @@
-function Home() {
+import "../../css/Dashboard/Home.scss";
+import { Image, Button} from "react-bootstrap";
+
+const Home = (props) => {
+  // get new date object
+  const date = new Date();
+  // get day in Intl format
+  const day = new Intl.DateTimeFormat("en-US", {weekday: "long"}).format(date);
+  // get month in Intl format
+  const month = new Intl.DateTimeFormat("en-US", {month: "long"}).format(date);
     return (
-        <h1>test</h1>
+      <>
+        <div id="dashboardHomeContainer">
+
+          <div id="dashboardWelcomeGrid">
+            <div id="dashboardHomeWelcome">
+              <p>Hello User</p>
+              <h1>Welcome to your Dashboard</h1>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci delectus deserunt accusamus inventore dolor expedita.</p>
+            </div>
+            <div id="dashboardHomeDay">
+              <p>{day}, {month}</p>
+              <h1>{date.getDay()}</h1>
+              <h3>{date.getFullYear()}</h3>
+            </div>
+          </div>
+
+          <div id="dashboardHomeMacros">
+            <div className="macro">
+              <Image src='https://placehold.co/0' roundedCircle />
+              <h1>Carbs</h1>
+            </div>
+            <div className="macro">
+              <Image src='https://placehold.co/0' roundedCircle />
+              <h1>Protein</h1>
+            </div>
+            <div className="macro">
+              <Image src='https://placehold.co/0' roundedCircle />
+              <h1>Fat</h1>
+            </div>
+          </div>
+
+          <div id="dashboardHomeButtons">
+              <Button onClick={() => {props.setKey("logFood")}}>Add New Food</Button>
+              <Button onClick={() => {props.setKey("myMeals")}}>My Meals</Button>
+          </div>
+        </div>
+      </>
     )
 }
 
